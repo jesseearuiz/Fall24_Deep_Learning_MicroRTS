1e65186c1eba28dcb35681f4143f49cc92cdb115
Julian Pandelakis <julianpandelakis@Julians-MacBook-Pro.local>
2024-12-13 16:31:59 -0500

    added new folder, deleted some files

---
+++ experiments/JLP/transformer_test/_mrts_utils.py
@@ -0,0 +1,20 @@
+import torch
+''' compact_observation_space takes in a tensor of batch_size, height * width, 29 and returns the
+same with final dim reduced to 6'''
+
+def compact_observation_space(x):
+	split_dims = [5,5,3,8,6,2]
+	split_dims = torch.split(x, split_dims, dim=-1)
+	new = []
+	for dim in split_dims:
+		new.append(torch.argmax(dim, dim=-1, keepdim=True))
+	return torch.cat(new, dim=-1)
+
+def main():
+	x = torch.randn((1, 16, 29))
+	print(compact_observation_space(x).shape)
+	print(compact_observation_space(x))
+	# Expected output: torch.Size([1, 16, 6])
+
+if __name__ == "__main__":
+	main()
